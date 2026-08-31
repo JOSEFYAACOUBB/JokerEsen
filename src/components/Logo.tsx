b70f7a92142510316847a8f5651336b7825ba8f2
JOSEFYAACOUBB <youssef.dj003@gmail.com>
2026-09-01 00:46:18 +0100

    adazdad

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -23,16 +23,7 @@ export const Logo: React.FC<LogoProps> = ({ className = '', showText = true, siz
         alt="JokerEsen Logo"
         className={`${currentSize.imgHeight} w-auto object-contain transition-transform duration-300 hover:scale-105 filter drop-shadow-lg`}
       />
-      {showText && (
-        <div className="flex flex-col leading-none">
-          <span className={`font-black tracking-tight ${currentSize.text} text-[#F5EDE4] font-display uppercase`}>
-            JOKER<span className="text-[#B93A34]">ESEN</span>
-          </span>
-          <span className={`${currentSize.subtext} font-bold uppercase tracking-widest text-[#F3C4A0]`}>
-            Club ESEN • Est. 2016
-          </span>
-        </div>
-      )}
+      
     </div>
   );
 };
